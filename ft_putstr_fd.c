981121e2b5a27f5e3963168ffbcd93ee144f5aba
youssef <youssef02benchaaboun@gmail.com>
2024-11-21 15:19:23 +0000

    finish

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -11,11 +11,10 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
 
 void	ft_putstr_fd(char *s, int fd)
 {
 	if (s == NULL)
 		return ;
-	write(fd, s, strlen(s));
+	write(fd, s, ft_strlen(s));
 }
